5307455e39444e24341a92d6f56ebb9b5d4757d7
Felipe Navarro Balbino Alves <fnba@cesar.org.br>
2017-06-22 17:42:41 -0300

    Fixed header bug on csv_read and added function to make histograms

--- util/csv_read.R
+++ util/csv_read.R
@@ -1,5 +1,9 @@
 load_data <- function(file_name){
-  data <- read.csv(file_name, sep=",", row.names=1)
+  data <- read.csv(file_name, sep=";", header = TRUE)
   data_matrix <- as.matrix(data)
   return(data_matrix)
+}
+
+make_hist <- function(data, column_name){
+  barplot(table(data[, column_name]))
 }
